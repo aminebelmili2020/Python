9700e23f3765f1fc0add66c01efbaddb0a31c0e0
aminebelmili2020 <aminelearning1@gmail.com>
2022-07-15 11:02:57 +0100

    first commit

---
+++ GeneratePassword.py
@@ -0,0 +1,11 @@
+from lib2to3.pygram import Symbols
+import random
+import string
+
+letters=string.ascii_letters
+num='0123456789'
+spe='-+*&!%$_'
+symbols=letters+num+spe
+len=int(input("Enter Pass. length: "))
+password="".join(random.sample(symbols, len))
+print(password)
